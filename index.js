8018e8efabbbfb7e99bb5954abcea139c75fd0f8
Thesupernile <thesupernile6@gmail.com>
2025-08-09 17:00:05 +0100

    Added dictionary and rearranged files

--- index.js
+++ index.js
@@ -1,4 +1,34 @@
-var text = "";
+var paragraphText = "";     // Stores the sequence of keys that the user has pressed in the past
 
 // Create the pairings between letters and images
 const letterImageMap = new Map();
+letterImageMap.set('q', "");
+letterImageMap.set('w', "");
+letterImageMap.set('e', "");
+letterImageMap.set('r', "");
+letterImageMap.set('t', "");
+letterImageMap.set('y', "");
+letterImageMap.set('u', "");
+letterImageMap.set('i', "");
+letterImageMap.set('o', "");
+letterImageMap.set('p', "");
+
+letterImageMap.set('a', "");
+letterImageMap.set('s', "");
+letterImageMap.set('d', "");
+letterImageMap.set('f', "");
+letterImageMap.set('g', "");
+letterImageMap.set('h', "");
+letterImageMap.set('j', "");
+letterImageMap.set('k', "");
+letterImageMap.set('l', "");
+
+letterImageMap.set('z', "");
+letterImageMap.set('x', "");
+letterImageMap.set('c', "");
+letterImageMap.set('v', "");
+letterImageMap.set('b', "");
+letterImageMap.set('n', "");
+letterImageMap.set('m', "");
+letterImageMap.set(',', "");
+
