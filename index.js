98ea1774fb280084f33b3527be8651112548e832
ethan-hawksley <136483392+ethan-hawksley@users.noreply.github.com>
2025-08-09 14:33:57 -0400

    Enhance index.html and index.js with new UI elements and connection logic

--- index.js
+++ index.js
@@ -1,54 +1,56 @@
-import {Peer} from "https://esm.sh/peerjs@1.5.5?bundle-deps"
+import {Peer} from "https://esm.sh/peerjs@1.5.5?bundle-deps";
 
 const peer = new Peer();
 let conn = null;
-document.getElementById('submit-connection').addEventListener('click',()=>{
-    const code = document.getElementById('connection-input').value;
-    peer.connect('symbolnotes' + code)
+let code = "";
+const submitConnection = document.getElementById("submit-connection");
+const connectionInput = document.getElementById("connection-input");
 
-    peer.on('open', () => {
-      document.getElementById('submit-connection').classList.add('hidden')
-    })
-})
+submitConnection.addEventListener("click", () => {
+  peer.connect("symbolnotes" + code);
+  peer.on("open", () => {
+    submitConnection.classList.add("hidden");
+  });
+});
 
 var paragraphText = "";     // Stores the sequence of keys that the user has pressed in the past
 
 // Create the pairings between letters and images
 const letterImageMap = new Map();
-letterImageMap.set('q', "symbols/notebook.png");
-letterImageMap.set('w', "symbols/stop.png");
-letterImageMap.set('e', "");
-letterImageMap.set('r', "");
-letterImageMap.set('t', "");
-letterImageMap.set('y', "");
-letterImageMap.set('u', "");
-letterImageMap.set('i', "");
-letterImageMap.set('o', "");
-letterImageMap.set('p', "");
+letterImageMap.set("q", "symbols/notebook.png");
+letterImageMap.set("w", "symbols/stop.png");
+letterImageMap.set("e", "");
+letterImageMap.set("r", "");
+letterImageMap.set("t", "");
+letterImageMap.set("y", "");
+letterImageMap.set("u", "");
+letterImageMap.set("i", "");
+letterImageMap.set("o", "");
+letterImageMap.set("p", "");
 
-letterImageMap.set('a', "");
-letterImageMap.set('s', "");
-letterImageMap.set('d', "");
-letterImageMap.set('f', "");
-letterImageMap.set('g', "");
-letterImageMap.set('h', "");
-letterImageMap.set('j', "");
-letterImageMap.set('k', "");
-letterImageMap.set('l', "");
+letterImageMap.set("a", "");
+letterImageMap.set("s", "");
+letterImageMap.set("d", "");
+letterImageMap.set("f", "");
+letterImageMap.set("g", "");
+letterImageMap.set("h", "");
+letterImageMap.set("j", "");
+letterImageMap.set("k", "");
+letterImageMap.set("l", "");
 
-letterImageMap.set('z', "symbols/red.png");
-letterImageMap.set('x', "symbols/orange.png");
-letterImageMap.set('c', "symbols/yellow.png");
-letterImageMap.set('v', "symbols/green.png");
-letterImageMap.set('b', "symbols/blueLight.png");
-letterImageMap.set('n', "symbols/blueDark.png");
-letterImageMap.set('m', "symbols/purple.png");
-letterImageMap.set(',', "symbols/pink.png");
+letterImageMap.set("z", "symbols/red.png");
+letterImageMap.set("x", "symbols/orange.png");
+letterImageMap.set("c", "symbols/yellow.png");
+letterImageMap.set("v", "symbols/green.png");
+letterImageMap.set("b", "symbols/blueLight.png");
+letterImageMap.set("n", "symbols/blueDark.png");
+letterImageMap.set("m", "symbols/purple.png");
+letterImageMap.set(",", "symbols/pink.png");
 
 
 // Register when a key is pressed and add it to the paragraphText
-document.body.onkeydown = function(key){
+document.body.onkeydown = function (key) {
 
-    paragraphText += key.value;
-    console.log(paragraphText);
-}
+  paragraphText += key.value;
+  console.log(paragraphText);
+};
